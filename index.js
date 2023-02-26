0e0e076cab1e0d428430ca8842d9d47ada4d9af7
Yash Kaka <kakayash02@gmail.com>
2023-02-26 15:16:26 +0000

    added backend

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+// import authRoute from "./routes/auth.js";
+// import usersRoute from "./routes/users.js";
+// import hotelsRoute from "./routes/hotels.js";
+// import roomsRoute from "./routes/rooms.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const connect = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://yash:yash@cluster0.8lyiexb.mongodb.net/?retryWrites=true&w=majority');
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
